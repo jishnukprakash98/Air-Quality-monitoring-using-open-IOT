2a6cd0aa5fc9962574fcdb0afaa96c8d8e718522
Jishnu <31334731+jishnukprakash98@users.noreply.github.com>
2018-08-01 19:54:44 +0530

    Add files via upload

---
+++ Air_pollution/libraries/hackAir/examples/Serial-PlainText/Serial-PlainText.ino
@@ -0,0 +1,42 @@
+/**
+ * @file Serial Sensor example
+ * This example reads data from a sensor (default choice is the serial SDS011)
+ * and sends it to a computer using USB serial communications.
+ *
+ * This example is part of the hackAIR Arduino Library and is available
+ * in the Public Domain.
+ */
+
+#include "hackair.h"
+
+// Specify your sensor
+hackAIR sensor(SENSOR_PMS5003);
+
+void setup() {
+  // Initialize the sensor
+  sensor.begin();
+
+  // Open serial port
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Take a reading
+  struct hackAirData data;
+  sensor.readData(data);
+
+  // If it was invalid, print error
+  if (data.error != 0) {
+    Serial.println("Error!");
+  } else {
+    // Print the values to serial
+    Serial.print("PM2.5: ");
+    Serial.println(data.pm25);
+    Serial.print("PM10: ");
+    Serial.println(data.pm10);
+    Serial.println("---");
+  }
+
+  // Wait a bit so the data is readable
+  delay(2500);
+}
